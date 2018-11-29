4f5e6c97a9d07e3bf2983a8d2cf63ae99f1fe3e7
Rclark4598 <rickdclark48@gmail.com>
2018-11-29 14:16:15 -0700

    randoms

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,63 +3,63 @@ var friends = [
         "name": "Ricky Rick",
         "picture": "https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTM2NTg1NzQz/tom-cruise-9262645-1-402.jpg",
         "answers": [
+            1,
             3,
+            1,
+            4,
             5,
-            1,
-            4,
-            3,
             2,
-            1,
-            3,
             4,
-            1
+            1,
+            2,
+            5
         ]
     },
     {
         "name": "Bob Belcher",
         "picture": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/3/3e/Bobbelcher2.png/revision/latest?cb=20130114075133",
         "answers": [
-            3,
+            2,
+            4,
+            4,
+            2,
             5,
             1,
-            4,
-            3,
-            2,
             1,
+            2,
             3,
-            4,
-            1
+            5
         ]
     },
     {
         "name": "Gene Belcher",
         "picture": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Gene_Belcher.png/revision/latest?cb=20130114043308",
         "answers": [
-            3,
             5,
-            1,
-            4,
-            3,
-            2,
-            1,
-            3,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     },
     {
         "name": "Tina Belcher",
         "picture": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/6/6e/Tina_render.png/revision/latest?cb=20130113192209",
         "answers": [
-            3,
-            5,
-            1,
+            4,
+            4,
             4,
             3,
-            2,
-            1,
             3,
-            4,
+            3,
+            1,
+            1,
+            1,
             1
         ]
     },
@@ -67,15 +67,15 @@ var friends = [
         "name": "Linda Belcher",
         "picture": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Lindab.png/revision/latest?cb=20130113071354",
         "answers": [
-            3,
-            5,
-            1,
-            4,
-            3,
             2,
-            1,
-            3,
-            4,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
             1
         ]
     },
@@ -83,16 +83,16 @@ var friends = [
         "name": "Louise Belcher",
         "picture": "https://banner2.kisspng.com/20180802/ayt/kisspng-clip-art-louise-belcher-cartoon-drawing-illustrati-louise-belcher-by-bishop1-618-on-deviantart-5b637f3e1d91d3.7490076815332472941211.jpg",
         "answers": [
-            3,
-            5,
-            1,
             4,
-            3,
-            2,
-            1,
-            3,
             4,
-            1
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
         ]
     },
 
